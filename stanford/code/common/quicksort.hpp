3f4aff495d1e33e2b4a356c4b660799dbd1b73f8
Dima Kunin <mr.kunin@gmail.com>
2014-10-25 23:48:05 +0300

    custom choosePivot function

--- stanford/code/common/quicksort.hpp
+++ stanford/code/common/quicksort.hpp
@@ -8,9 +8,35 @@ using std::swap;
 
 namespace alg
 {
+
     template <typename T>
-    size_t partition(vector<T> & vec, size_t left, size_t right) {
-        T pivot = vec[left]; // TODO add different choosePivot()
+    size_t chooseLeft(vector<T> & vec, size_t left, size_t right)
+    {
+        return vec[left];
+    }
+
+    template <typename T>
+    size_t chooseRight(vector<T> & vec, size_t left, size_t right)
+    {
+        return vec[right];
+    }
+
+    template <typename T>
+    size_t chooseMedianOfThree(vector<T> & vec, size_t left, size_t right)
+    {
+        // TODO
+        return vec[left];
+    }
+
+    template <typename T>
+    size_t partition(
+            vector<T> & vec,
+            size_t left,
+            size_t right,
+            size_t (*choosePivotFunc)(vector<T>&, size_t, size_t) = &chooseLeft)
+    {
+        T pivot = choosePivotFunc(vec, left, right);
+
         size_t border = left + 1;
         for (size_t j = left + 1; j <= right; ++j)
         {
@@ -31,6 +57,7 @@ namespace alg
         if (vec_size < 2) return;
 
         size_t pivotIndex = partition(vec, left, right);
+
         quicksort(vec, left, pivotIndex - 1);
         quicksort(vec, pivotIndex + 1, right);
     }
